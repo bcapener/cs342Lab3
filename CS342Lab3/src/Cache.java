0b17ca80f1c6de966024e714ab2e9deb49f1c462
Daniel De La Garza <danieldlgrz@gmail>
2013-12-04 23:06:19 -0700

    Added my cahce class

---
+++ CS342Lab3/src/Cache.java
@@ -0,0 +1,110 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Class that acts as a cache using a linked list given a max cache size.
+ * @author daniel
+ *
+ * @param <T>
+ */
+public class Cache<T> {
+	private LinkedList<T> list;
+	private final int MAXSIZE;
+	
+	/**
+	 * Constructor for the Cache class.
+	 * @param maxSize Max size of the cache object.
+	 */
+	public Cache(int maxSize){
+		this.MAXSIZE = maxSize;
+		list = new LinkedList<T>();
+	}
+	
+	/**
+	 * Adds object to the head of the list. Removes last object(s) if the list 
+	 * exceeds max size
+	 * @param o Object to add at the head of the list.
+	 */
+	public void addObject(T o){
+		list.addFirst((T) o);
+		while(list.size() > MAXSIZE){
+			list.removeLast();
+		}
+		
+	}
+	
+	/**Removes first instance of an object in the list.
+	 * 
+	 * @param o Object to remove from list.
+	 * @return Returns true if the object is found and removed.
+	 */
+	public boolean removeObject(T o){
+		return(list.remove(o));
+	}
+	
+	/**
+	 * Clears the cache list.
+	 */
+	public void clearCache(){
+		list.clear();
+	}
+	
+	/**
+	 * 
+	 * @return Returns current size of cache.
+	 */
+	public int getSize(){
+		return list.size();
+	}
+	
+	/**
+	 * 
+	 * @return Returns max size of the cache/
+	 */
+	public int getMaxSize(){
+		return MAXSIZE;
+	}
+	
+	/**
+	 * 
+	 * @return Returns true if cache is full (current size is equal or exceeds max size).
+	 */
+	public boolean isFull(){
+		return list.size() >= MAXSIZE;
+	}
+	
+	/**
+	 * 
+	 * @param o Object to find.
+	 * @return returns first instance of an object in the list.
+	 */
+	public T getObject(T o){
+		Iterator<T> iterator = list.iterator();
+		while(iterator.hasNext()){
+			T object = iterator.next();
+			if(object.equals(o)){
+				return object;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns object at an index.
+	 * @param i Index of object
+	 * @return Returns object in list at index i.
+	 */
+	public T getObjectAtIndex(int i){
+		return list.get(i);
+	}
+	
+	/**
+	 * Removes object at an index.
+	 * @param i Index of object
+	 * @return Returns object removed at index i of list.
+	 */
+	public T removeObjectAtIndex(int i){
+		return list.remove(i);
+	}
+
+}
