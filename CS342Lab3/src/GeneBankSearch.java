6bf04ef23becbb71421afbcf58872a3e7db61615
bcapener <bcapener@node20.boisestate.edu>
2013-12-05 14:07:45 -0700

    n

--- CS342Lab3/src/GeneBankSearch.java
+++ CS342Lab3/src/GeneBankSearch.java
@@ -1,4 +1,5 @@
 
 public class GeneBankSearch {
-
+	//java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]
+	//java GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]
 }
